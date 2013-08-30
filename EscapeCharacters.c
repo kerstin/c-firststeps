05342fe0ef2b9759984951ec9aa8befcb9fe1e77
Kerstin Kollmann <code@kerstinkollmann.com>
2013-08-30 16:12:18 +0200

    first c programmmmmmmmmmmm

---
+++ EscapeCharacters.c
@@ -0,0 +1,28 @@
+/*	Write a program that produces the following output on the console:
+
+	escape sequence is started with a backslash: \
+	for printing a tabulator		use "\t"
+	for printing a backslash	(\)	use "\\"
+	for printing double quotes	(")	use "\""
+	for printing a single quote	(')	use "\'"
+	for starting a new line ->
+	->							use "\n"
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	printf("escape sequence is started with a backslash: \\\n");
+	printf("for printing a tabulator\t\tuse \"\\t\"\n");
+	printf("for printing a backslash\t(\\)\tuse \"\\\\\"\n");
+	printf("for printing double quotes\t(\")\tuse \"\\\"\"\n");
+	printf("for printing a single quote\t(\')\tuse \"\\\'\"\n");
+	printf("for starting a new line ->\n");
+	printf("->\t\t\t\tuse \"\\n\"\n");
+
+	return 0;
+
+}
+
