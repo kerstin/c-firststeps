9ed2c353cddd52f69216e59c3f47609bec9484da
Kerstin Kollmann <code@kerstinkollmann.com>
2013-10-08 01:15:37 +0200

    cleaned up code

--- TempConverter.c
+++ TempConverter.c
@@ -1,10 +1,10 @@
-/*	Write a program that displays a temperature
-	conversion table. The table starts at -100°C
-	and goes up to 100°C with a step size of 10°C.
+/*  Write a program that displays a temperature
+    conversion table. The table starts at -100°C
+    and goes up to 100°C with a step size of 10°C.
 
-	Use the following formulas for conversion:
-	C->F: F = C x 1.8 + 32
-	C->K: K = C + 273.15
+    Use the following formulas for conversion:
+    C->F: F = C x 1.8 + 32
+    C->K: K = C + 273.15
 */
 
 #include <stdio.h>
@@ -12,30 +12,19 @@
 
 int main() {
 
-	float celsius = -100;
-	float fahrenheit;
-	float kelvin;
+    float celsius = -100;
+    float fahrenheit;
+    float kelvin;
 
-	printf("Celsius\t\tKelvin\t\tFahrenheit\n");
+    printf("Celsius\t\tKelvin\t\tFahrenheit\n");
 
-	// // while loop (not-so-nice solution)
-	// while(celsius<=100) {
-	// 	// C->F: F = C x 1.8 + 32
-	// 	fahrenheit = celsius * 1.8 + 32;
-	// 	// C->K: K = C + 273.15
-	// 	kelvin = celsius + 273.15;
-	// 	printf("%.1f\t\t%.2f\t\t%.f\n", celsius, kelvin, fahrenheit);
-	// 	celsius = celsius + 10;
-	// }
+    for(celsius=-100; celsius<=100; celsius+=10) {
+        // C->F: F = C x 1.8 + 32
+        fahrenheit = celsius * 1.8 + 32;
+        // C->K: K = C + 273.15
+        kelvin = celsius + 273.15;
+        printf("%.1f\t\t%.2f\t\t%.f\n", celsius, kelvin, fahrenheit);
+    }
 
-	// for loop - nicer solution! (;
-	for(celsius=-100; celsius<=100; celsius+=10) {
-		// C->F: F = C x 1.8 + 32
-		fahrenheit = celsius * 1.8 + 32;
-		// C->K: K = C + 273.15
-		kelvin = celsius + 273.15;
-		printf("%.1f\t\t%.2f\t\t%.f\n", celsius, kelvin, fahrenheit);
-	}
-
-	return 0;
+    return 0;
 }
