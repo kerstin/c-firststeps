1872102ba3b2b7079a9bedf0eea03ef8ef0b68e7
Kerstin Kollmann <code@kerstinkollmann.com>
2013-10-08 01:38:03 +0200

    created new program Seconds2MinutesHours

---
+++ Seconds2MinutesHours.c
@@ -0,0 +1,15 @@
+/*  Write a program that converts seconds to minutes and hours. The user enters the value in seconds, the program calculates the number of minutes and hours and outputs the result.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int userinput = 0;
+
+    printf("Enter seconds to convert into minutes and hours: ");
+    scanf("%d", &userinput);
+
+    return 0;
+}
