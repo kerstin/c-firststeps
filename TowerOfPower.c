ff136a88c0c47ba38ab12bfcf674f6c0a815172a
Kerstin Kollmann <code@kerstinkollmann.com>
2013-10-08 02:00:58 +0200

    wrapped lines

--- TowerOfPower.c
+++ TowerOfPower.c
@@ -1,4 +1,6 @@
-/*  Write a program TowerOfPower that asks the user for an int value between 1 and 10 that serves as the base value. Display the 7 lowest powers of the base value. Display them right-justified.
+/*  Write a program TowerOfPower that asks the user for an int value
+between 1 and 10 that serves as the base value. Display the 7 lowest
+powers of the base value. Display them right-justified.
 
 It is not necessary to check whether input is in the expected range.
 */
@@ -10,7 +12,8 @@ int main() {
 
     int userinput = 0;
 
-    printf("Please enter a number between 1 and 10\nto build a 'Tower of Power' from its first seven powers: ");
+    printf("Please enter a number between 1 and 10\nto build a "
+        "'Tower of Power' from its first seven powers: ");
     scanf("%d", &userinput);
 
     printf("\t%8d\n", userinput);
@@ -18,8 +21,10 @@ int main() {
     printf("\t%8d\n", userinput*userinput*userinput);
     printf("\t%8d\n", userinput*userinput*userinput*userinput);
     printf("\t%8d\n", userinput*userinput*userinput*userinput*userinput);
-    printf("\t%8d\n", userinput*userinput*userinput*userinput*userinput*userinput);
-    printf("\t%8d\n", userinput*userinput*userinput*userinput*userinput*userinput*userinput);
+    printf("\t%8d\n", userinput*userinput*userinput*userinput*userinput*
+        userinput);
+    printf("\t%8d\n", userinput*userinput*userinput*userinput*userinput*
+        userinput*userinput);
 
     return 0;
 }
